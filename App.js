2923abb1ab6a011ee822c42fbe01b7e206b78228
Dimitar Mitev <d.mitev920123@gmail.com>
2023-09-30 20:20:09 +0300

    Setup google fonts

--- App.js
+++ App.js
@@ -2,13 +2,36 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular
+} from '@expo-google-fonts/oswald';
+
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+  Lato_700Bold
+} from '@expo-google-fonts/lato';
+
 import { theme } from './src/infrastructure/theme/index.js';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen.js';
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+    Lato_700Bold
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    null;
+  }
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme} >
         <RestaurantsScreen />
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
