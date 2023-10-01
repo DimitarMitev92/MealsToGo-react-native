ec83fc2d7210a36b97c8e2107fb55b30255fecea
Dimitar Mitev <d.mitev920123@gmail.com>
2023-10-01 16:16:10 +0300

    Spacer component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,6 +4,7 @@ import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
+import { Spacer } from "../../../components/spacer/spacer.component.js";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
@@ -47,45 +48,45 @@ const SectionEnd = styled.View`
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
-    const {
-        name = "Some Restaurant",
-        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-        photos = [
-            "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-        ],
-        address = "100 some random street",
-        isOpenNow = true,
-        rating = 4,
-        isClosedTemporarily = true,
-    } = restaurant;
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
 
-    const ratingArray = Array.from(new Array(Math.floor(rating)));
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
-    return (
-        <RestaurantCard elevation={5}>
-            <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Info>
-                <Title>{name}</Title>
-                <Section>
-                    <Rating>
-                        {ratingArray.map(() => (
-                            <SvgXml xml={star} width={20} height={20} />
-                        ))}
-                    </Rating>
-                    <SectionEnd>
-                        {isClosedTemporarily && (
-                            <Text variant="label" style={{ color: "red" }}>
-                                CLOSED TEMPORARILY
-                            </Text>
-                        )}
-                        <View style={{ paddingLeft: 16 }} />
-                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-                        <View style={{ paddingLeft: 16 }} />
-                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-                    </SectionEnd>
-                </Section>
-                <Address>{address}</Address>
-            </Info>
-        </RestaurantCard>
-    );
-};
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer variant="left.medium" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.small" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+}; 
