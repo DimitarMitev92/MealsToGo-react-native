0840cdfe58302e6e8ce3d461b8de1e2dff8fce92
Dimitar Mitev <d.mitev920123@gmail.com>
2023-10-02 16:10:08 +0300

    Tab Navigation

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Text } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {
   useFonts as useOswald,
@@ -15,6 +18,11 @@ import {
 
 import { theme } from './src/infrastructure/theme/index.js';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen.js';
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => <Text>Settings</Text>;
+const Map = () => <Text>Map</Text>;
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -32,7 +40,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme} >
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Settings} />
+            <Tab.Screen name="Settings" component={Map} />
+          </Tab.Navigator>
+        </NavigationContainer>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </>
